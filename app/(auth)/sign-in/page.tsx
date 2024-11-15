4ade854d83091c89a863952aa040d258cc82b40b
Pranav Bhalerao <pranav2003bhalerao@gmail,com>
2024-11-15 20:05:44 +0530

    Authentication Layout Complete

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const SignIn = () => {
+  return <div>Sign In</div>;
+};
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const SignUp = () => {
+  return <div>Sign Up</div>;
+};
+export default SignUp;
